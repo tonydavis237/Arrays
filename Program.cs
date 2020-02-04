fc0895e254f1dd3ff34b6f680730182b3eca44d6
Tony Davis <anthony79@mail.usf.edu>
2020-02-04 18:49:17 -0500

    Commit all files

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * Author: Anthony Davis
+ * Date: 02/06/2020
+ * Description: Creating an automatic array
+ */ 
+using System;
 
 namespace Arrays
 {
@@ -6,7 +11,21 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Creating an array to display the numbers 1-25
+            int[] numberArray = new int[25];
+
+            // Creating an integer value in order to show the iteration the loop will be in
+            int popArray = 1;
+
+            // For loop to display all the elements in the array
+            for(int count = 0; count < 25; count++)
+            {
+                numberArray[count] = popArray;
+                Console.WriteLine("Element value = " + popArray.ToString());
+                popArray++;
+            }
+            // Allows the user to exit the program
+            Console.ReadKey(true);
         }
     }
 }
